39275c71d52416632d425b381018f33e114157d7
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2018-01-09 13:14:10 +0200

    Phone input changes

--- src/lib/src/component/int-phone-prefix/int-phone-prefix.component.ts
+++ src/lib/src/component/int-phone-prefix/int-phone-prefix.component.ts
@@ -144,12 +144,12 @@ export class IntPhonePrefixComponent implements OnInit, ControlValueAccessor {
     private updatePhoneInput(countryCode: string) {
         this.showDropdown = false;
 
-        // let newInputValue: string = IntPhonePrefixComponent.startsWithPlus(this.phoneInput)
-        //     ? `${this.phoneInput.split(PLUS)[1].substr(this.selectedCountry.dialCode.length, this.phoneInput.length)}`
-        //     : this.phoneInput;
-        //
+        let newInputValue: string = IntPhonePrefixComponent.startsWithPlus(this.phoneInput)
+            ? `${this.phoneInput.split(PLUS)[1].substr(this.selectedCountry.dialCode.length, this.phoneInput.length)}`
+            : this.phoneInput;
+
         // this.selectedCountry = this.countries.find((country: Country) => country.countryCode === countryCode);
-        // this.phoneInput = `${PLUS}${this.selectedCountry.dialCode} ${newInputValue.replace(/ /g, '')}`;
+        this.phoneInput = newInputValue;
     }
 
     private findPrefix(prefix: string) {
